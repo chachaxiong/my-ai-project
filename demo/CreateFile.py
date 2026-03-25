cf5e286eee923d27b8212d5b39a8acf0d970ea1e
chachaxiong <ligangjian0516@163.com>
2026-03-25 16:59:54 +0800

    删除无用代码

--- demo/CreateFile.py
+++ demo/CreateFile.py
@@ -14,7 +14,6 @@ def readFile(name, content):
 
 def writeFileToCsv(name, content):
     file_exists = os.path.exists('test.csv')
-    
     with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         if not file_exists:
@@ -23,5 +22,5 @@ def writeFileToCsv(name, content):
     print(f"数据已追加到 test.csv")
 
 if __name__ == '__main__':
-    readFile("test.txt", "Hello World")
+    # readFile("test.txt", "Hello World")
     writeFileToCsv("示例文件", "这是一段测试内容")
